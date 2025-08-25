bd0612694b74a17c15cead56d9ed806f2c7a89f4
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-08-25 21:57:05 +0200

    feat: create simple from

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
 export default function App() {
-  return <h1>Hello PocketList </h1>;
+  return (
+    <form>
+      <label htmlFor="title">
+        <input id="title" name="title" />
+      </label>
+
+      <label htmlFor="priority">
+        <select id="priority" name="priority">
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        </select>
+      </label>
+
+      <button type="submit">Add Task</button>
+    </form>
+  );
 }
