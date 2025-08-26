05ea29dad0a62fe3e09ac3b8fe11d3315234a307
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-08-26 08:03:04 +0200

    feat: implement handleChange function which manages input change

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
+import { useState } from "react";
+
 export default function App() {
+  const initialTask = { title: "", priority: "low" };
+  const [newTask, setNewTask] = useState(initialTask);
+  const [tasks, setTasks] = useState([]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setNewTask((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   return (
-    <form>
-      <label htmlFor="title">
-        <input id="title" name="title" />
-      </label>
+    <>
+      <form>
+        <label htmlFor="title">
+          <input
+            id="title"
+            name="title"
+            value={newTask.title}
+            onChange={handleChange}
+          />
+        </label>
 
-      <label htmlFor="priority">
-        <select id="priority" name="priority">
-          <option value="low">Low</option>
-          <option value="medium">Medium</option>
-          <option value="high">High</option>
-        </select>
-      </label>
+        <label htmlFor="priority">
+          <select id="priority" name="priority">
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+          </select>
+        </label>
 
-      <button type="submit">Add Task</button>
-    </form>
+        <button type="submit">Add Task</button>
+      </form>
+
+      {newTask.title && console.log(newTask)}
+    </>
   );
 }
