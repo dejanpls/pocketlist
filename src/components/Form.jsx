2e7be16a9a2e992a6187bd7e538284f4779dec4a
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-08-31 21:22:47 +0200

    style: quantity input, priority select and buttons

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -20,54 +20,61 @@ export default function Form({
           />
         </label>
       </div>
+      <div className="controls">
+        <div className="secondary-input-container">
+          <label htmlFor="quantity">
+            Quantity:
+            <input
+              type="number"
+              min="1"
+              max="99999"
+              id="quantity"
+              name="quantity"
+              value={newTask.quantity}
+              onChange={handleChange}
+            />
+          </label>
 
-      <label htmlFor="quantity">
-        <input
-          type="number"
-          min="1"
-          max="99999"
-          id="quantity"
-          name="quantity"
-          value={newTask.quantity}
-          onChange={handleChange}
-        />
-      </label>
+          <label htmlFor="priority">
+            Priority:
+            <select
+              id="priority"
+              name="priority"
+              value={newTask.priority}
+              onChange={handleChange}
+            >
+              <option value="low">Low</option>
+              <option value="medium">Medium</option>
+              <option value="high">High</option>
+            </select>
+          </label>
+        </div>
 
-      <label htmlFor="priority">
-        <select
-          id="priority"
-          name="priority"
-          value={newTask.priority}
-          onChange={handleChange}
-        >
-          <option value="low">Low</option>
-          <option value="medium">Medium</option>
-          <option value="high">High</option>
-        </select>
-      </label>
+        <div className="btn-container">
+          <button
+            type="submit"
+            disabled={
+              !newTask.title ||
+              (editTask &&
+                newTask.title === editTask.title &&
+                newTask.quantity === editTask.quantity &&
+                newTask.priority === editTask.priority)
+            }
+          >
+            {editId ? "Save Changes" : "Add Task"}
+          </button>
 
-      <button
-        type="submit"
-        disabled={
-          !newTask.title ||
-          (editTask &&
-            newTask.title === editTask.title &&
-            newTask.quantity === editTask.quantity &&
-            newTask.priority === editTask.priority)
-        }
-      >
-        {editId ? "Save Changes" : "Add Task"}
-      </button>
-
-      {editId && (
-        <button type="button" onClick={handleCancel}>
-          {newTask.title === editTask.title &&
-          newTask.quantity === editTask.quantity &&
-          newTask.priority === editTask.priority
-            ? "Cancel"
-            : "Discard"}
-        </button>
-      )}
+          {editId && (
+            <button type="button" onClick={handleCancel}>
+              {newTask.title === editTask.title &&
+              newTask.quantity === editTask.quantity &&
+              newTask.priority === editTask.priority
+                ? "Cancel"
+                : "Discard"}
+            </button>
+          )}
+        </div>
+      </div>
     </form>
   );
 }
