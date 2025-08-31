3313c46cdab3fb484699eeba6457b87c6d29e39f
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-08-31 21:58:12 +0200

    style: task list

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,3 +1,5 @@
+import "../styles/taskList.css";
+
 export default function TaskList({
   getProcessedTasks,
   handleCompleted,
@@ -8,34 +10,44 @@ export default function TaskList({
   return (
     <>
       {getProcessedTasks().length > 0 ? (
-        <ul>
+        <ul className="taskList">
           {getProcessedTasks().map((task) => (
-            <li key={task.id}>
-              <input
-                type="checkbox"
-                checked={task.completed}
-                onChange={() => handleCompleted(task.id)}
-              />
-              <h2>{task.title}</h2>
-              <p>Quantity: {task.quantity}</p>
-              <p>
-                Priority:{" "}
-                {task.priority[0].toUpperCase() + task.priority.slice(1)}
-              </p>
-              <button
-                disabled={editId === task.id}
-                type="button"
-                onClick={() => handleDelete(task.id)}
-              >
-                Delete
-              </button>
-              <button
-                disabled={editId}
-                type="button"
-                onClick={() => handleEdit(task.id)}
-              >
-                Edit
-              </button>
+            <li key={task.id} className="listItem">
+              <div className="primary-container">
+                <input
+                  type="checkbox"
+                  id={task.id}
+                  checked={task.completed}
+                  onChange={() => handleCompleted(task.id)}
+                />
+                <h2>{task.title}</h2>
+              </div>
+
+              <div className="secondary-container">
+                <div className="secondary-info-container">
+                  <p>Quantity: {task.quantity}</p>
+                  <p>
+                    Priority:{" "}
+                    {task.priority[0].toUpperCase() + task.priority.slice(1)}
+                  </p>
+                </div>
+                <div className="btn-container">
+                  <button
+                    disabled={editId === task.id}
+                    type="button"
+                    onClick={() => handleDelete(task.id)}
+                  >
+                    Delete
+                  </button>
+                  <button
+                    disabled={editId}
+                    type="button"
+                    onClick={() => handleEdit(task.id)}
+                  >
+                    Edit
+                  </button>
+                </div>
+              </div>
             </li>
           ))}
         </ul>
